91347661bb88d18831c43f2467c90982f19e44ad
Z-100 <marvin131t@gmail.com>
2023-07-15 23:35:17 +0200

    feature(sidebar): Fix function call

--- src/components/common/ToggleSwitch.tsx
+++ src/components/common/ToggleSwitch.tsx
@@ -9,7 +9,7 @@ export const ToggleSwitch = ({ fun }: ToggleSwitchProps) => {
     return(
         <>
             <label className="switch">
-                <input type="checkbox" />
+                <input type="checkbox" onChange={fun} />
                 <span className="slider round"></span>
             </label>
         </>
