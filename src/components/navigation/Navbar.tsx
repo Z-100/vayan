0685c6b6507bd6822a548fb3a69d48d6fcaa47c8
Z-100 <marvin131t@gmail.com>
2024-02-12 11:57:30 +0100

    feature(page-layout): Add mobile stuff :)

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {groupRoutes} from "../../domain";
 import {routerData} from "../router";
 import {Link} from "react-router-dom";
@@ -7,23 +7,39 @@ export const Navbar = () => {
 
     const groupedRoutes = groupRoutes(routerData)
 
+    const [active, setActive] = useState(routerData[0].name)
+
     return (
-        <nav className="w-full h-16 p-8 bg-layerOne flex sm:rounded-b-none rounded-2xl"> // TODO: Basically the sm thingy
-            {Object.keys(groupedRoutes).map((groupName, index) =>
-                <React.Fragment key={groupName}>
-                   <span className="flex-grow flex">
-                        {groupedRoutes[groupName].map(route =>
-                            <Link key={route.name}
-                                  to={route.path}
-                                  className="flex items-center space-x-2">
-                                <route.icon/>
-                                <p>{route.name}</p>
-                            </Link>
-                        )}
-                    </span>
-                    {index < Object.keys(groupedRoutes).length - 1 && <span className="mx-2">|</span>}
-                </React.Fragment>
-            )}
-        </nav>
+        <>
+            <div
+                className="float flex items-center justify-center fixed bottom-0 left-0 right-0 mb-4 sm:top-0 sm:mb-32 sm:bottom-auto">
+                <nav className="bg-layerOne flex rounded-full w-64 h-12 sm:w-full sm:h-16 sm:p-8 sm:rounded-none">
+                    {Object.keys(groupedRoutes).map((groupName, index) =>
+                        <React.Fragment key={groupName}>
+                           <span className="flex-grow flex">
+                                {groupedRoutes[groupName].map(route =>
+                                    <Link key={route.name}
+                                          to={route.path}
+                                          onClick={() => setActive(route.name)}
+                                          className="flex items-center space-x-2">
+
+                                        <span className="visible sm:invisible">
+                                            <route.icon className={active === route.name ? "bg-primary rounded-full w-6 h-auto" : ""}/>
+                                        </span>
+
+                                        <span className="invisible sm:visible">
+                                            <p className={active === route.name ? "font-bold" : ""}>{route.name}</p>
+                                        </span>
+                                    </Link>
+                                    )}
+                            </span>
+                            {index < Object.keys(groupedRoutes).length - 1 && <span className="mx-2">|</span>}
+                        </React.Fragment>
+                    )}
+                </nav>
+            </div>
+            <div className="h-16"></div>
+        </>
+
     )
 }
