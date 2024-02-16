97397760d16e647e1b2f4063d6cf804ea9a6f573
Z-100 <marvin131t@gmail.com>
2024-02-16 11:48:45 +0100

    feature(page-layout): Lil navbar update

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -11,35 +11,32 @@ export const Navbar = () => {
 
     return (
         <>
-            <div
-                className="float flex items-center justify-center fixed bottom-0 left-0 right-0 mb-4 sm:top-0 sm:mb-32 sm:bottom-auto">
+            <div className="fixed bottom-0 left-0 right-0 mb-4 sm:top-0 sm:mb-32 sm:bottom-auto flex justify-center">
                 <nav className="bg-layerOne flex rounded-full w-64 h-12 sm:w-full sm:h-16 sm:p-8 sm:rounded-none">
-                    {Object.keys(groupedRoutes).map((groupName, index) =>
-                        <React.Fragment key={groupName}>
-                           <span className="flex-grow flex">
-                                {groupedRoutes[groupName].map(route =>
-                                    <Link key={route.name}
-                                          to={route.path}
-                                          onClick={() => setActive(route.name)}
-                                          className="flex items-center space-x-2">
+                    {Object.keys(groupedRoutes).map((groupName, index) => (
+                        <div key={groupName} className="w-full gap-1 flex flex-nowrap">
+                            {groupedRoutes[groupName].map(route => (
+                                <Link key={route.name}
+                                      to={route.path}
+                                      onClick={() => setActive(route.name)}
+                                      className="flex items-center">
 
-                                        <span className="visible sm:invisible">
-                                            <route.icon className={active === route.name ? "bg-primary rounded-full w-6 h-auto" : ""}/>
-                                        </span>
+                                    <span className="visible sm:invisible w-6">
+                                        <route.icon
+                                            className={active === route.name ? "bg-primary rounded-full w-auto h-auto" : ""}/>
+                                    </span>
 
-                                        <span className="invisible sm:visible">
-                                            <p className={active === route.name ? "font-bold" : ""}>{route.name}</p>
-                                        </span>
-                                    </Link>
-                                    )}
-                            </span>
+                                    <span className="invisible sm:visible">
+                                        <p className={active === route.name ? "font-bold" : ""}>{route.name}</p>
+                                    </span>
+                                </Link>
+                            ))}
                             {index < Object.keys(groupedRoutes).length - 1 && <span className="mx-2">|</span>}
-                        </React.Fragment>
-                    )}
+                        </div>
+                    ))}
                 </nav>
             </div>
-            <div className="h-16"></div>
+            <div className="h-16 hidden sm:block"></div>
         </>
-
     )
 }
