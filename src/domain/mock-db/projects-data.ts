bb802e318276e67ffda1bc08cc8e346d1c41f3ef
Z-100 <marvin131t@gmail.com>
2024-04-06 17:49:26 +0200

    feature(update-text-magic): Reorder images

--- src/domain/mock-db/projects-data.ts
+++ src/domain/mock-db/projects-data.ts
@@ -13,35 +13,35 @@ export const featuredProjectsData: Project[] = [
         shortDesc: 'Literal weather app...',
         desc: 'Camunda-, kTor- & React-Microservices running in AKS',
         url: 'https://github.com/Z-100/soemi-weather',
-        img: '/aks-logo.png'
+        img: '/logos/aks-logo.png'
     },
     {
         name: 'GeoPal',
         shortDesc: 'Geo Reminder App',
         desc: 'Basically a location based reminder app, where I also focused on the UI. I\'m definitely a backend dev',
         url: 'https://github.com/ZE-100/GeoPal',
-        img: '/android-logo.png'
+        img: '/logos/android-logo.png'
     },
     {
         name: 'BackFusion',
         shortDesc: 'Kotlin and Spring Boot',
         desc: 'The backend to a gym-tracking app written in Kotlin. More of an experiment.',
         url: 'https://github.com/FitFusion/BackFusion',
-        img: '/fitfusion-logo.jpg'
+        img: '/logos/fitfusion-logo.jpg'
     },
     {
         name: 'DayCounter',
         shortDesc: 'Cute day counter app',
         desc: 'One of my first Android apps! It counts down days in a cute way',
         url: 'https://github.com/ZE-100/DayCounter',
-        img: '/android-logo.png'
+        img: '/logos/android-logo.png'
     },
     {
         name: 'More projects',
         shortDesc: 'Wanna see more?',
         desc: 'There are lots of other projects on my GitHub!',
         url: 'https://github.com/Z-100?tab=repositories',
-        img: '/github-logo.png'
+        img: '/logos/github-logo.png'
     },
 ]
 
@@ -51,13 +51,13 @@ export const plannedProjectsData: Project[] = [
         shortDesc: "Document Management Zystem",
         desc: "I wanna create my own DMS-System to manage... documents 🥁",
         url: "https://github.com/Z-100/DMZ",
-        img: "/logo171.png"
+        img: "/logos/elmo-logo.png"
     },
     {
         name: "k8s",
         shortDesc: "Soon to be self hosted Kubernetes Cluster",
         desc: "All of my projects should run here eventually. That's the end goal.",
         url: "https://github.com/Z-100/k8s",
-        img: "/kubernetes-logo.png"
+        img: "/logos/kubernetes-logo.png"
     },
 ]
