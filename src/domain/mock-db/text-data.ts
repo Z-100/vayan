aeb4bea6ea948c2d3a85227d82f6865849b160ef
Z-100 <marvin131t@gmail.com>
2024-04-06 17:40:22 +0200

    feature(update-text-magic): Make texts multi functional by adding lists

--- src/domain/mock-db/text-data.ts
+++ src/domain/mock-db/text-data.ts
@@ -9,84 +9,95 @@ export const introTexts: TextSection[] = [
     },
 ]
 
-export const aboutWebsiteTexts: TextSection[] = [
+export const aboutMeTexts: TextSection[] = [
     {
-        title: 'What\'s this website about?',
-        text:
-            'TL;DR: Backend dev doing frontend\n' +
-            'br\n' +
-            'I\'m a passionate backend developer in need of a website.\n' +
-            'And what better way is there to cut costs - I mean to learn new stuff - than by doing it myself?\n' +
-            'br\n' +
-            'I\'m a backend dev who is currently learning how to frontend.\n' +
-            'So don\'t judge Vayan to hard :)',
+        title: 'Hi there, Marvin here',
+        text: 'I\'m a backend dev from Switzerland, the land of the cows and the cheese and the more cows',
+        subTexts: [
+            {
+                title: 'Stuff I like',
+                text: [
+                    'Motorcycles',
+                    'Programming',
+                    'Games',
+                    'Programming Games',
+                    'Food (non cheesey stuff as well)',
+                ]
+            },
+            {
+                title: 'I am...',
+                text: [
+                    '...a passionate backend developer in love with Spring Boot',
+                    '...trying my best with Vayan in the web',
+                    '...experimenting with Android apps, Arduino & Unity',
+                    '...looking for jobs (there\'s a contact form)',
+                    '...constantly learning new stuff',
+                ]
+            },
+        ],
     },
 ]
 
-export const aboutMeTexts: TextSection[] = [
+export const aboutWebsiteTexts: TextSection[] = [
     {
-        title: 'About me',
-        text: '',
-        texts: [
-            {
-                title: 'I am...',
-                text:
-                    '- 🗿  ... a passionate backend developer in love with Spring Boot\n' +
-                    '- 💻 ... trying my best with Vayan in the web\n' +
-                    '- 🎩 ...experimenting with Android-apps, Arduino & Unity\n' +
-                    '- 🚢 ...looking for jobs\n' +
-                    '- 🌈 ...constantly learning new stuff'
-            },
-        ],
+        title: 'What even is this website tho?',
+        text:
+            'TL;DR: Backend dev doing frontend\n' +
+            '\n' +
+            'I\'m a passionate backend developer in need of a website.\n' +
+            'And what better way is there to cut costs - I mean to learn new stuff - than by doing it myself?\n' +
+            '\n' +
+            'Basically, I\'m a backend dev who is currently learning how to frontend.\n' +
+            'So don\'t judge Vayan too hard :)',
     },
 ]
 
 export const skillsTexts: TextSection[] = [
     {
         title: 'Tech stack',
-        text: '',
-        texts: [
+        text: 'A rough overview of the stuff I\'ve used so far',
+        subTexts: [
             {
                 title: 'Java',
-                text:
-                    'I started writing Java back in 2021 and have been using it ever since.\n' +
-                    '> Both professionally and for my own projects\n' +
-                    'It\'s a good language and I like it, but...\n' +
-                    'br\n',
+                text: [
+                    'I started writing Java back in 2021 and have been using it ever since',
+                    'Both professionally and for my own projects',
+                    'It\'s a good language, and I like it, but...',
+                ],
             },
             {
                 title: 'Kotlin',
-                text:
-                    '> Easily my favourite language.\n' +
-                    'Mostly, I\'ve used Kotlin for Android apps\n' +
-                    'But as time went on I started using it in other use cases as well, e.g. Spring Boot\n' +
-                    '> Going forward I definitely wanna improve my skills in that language!\n' +
-                    'br\n',
+                text: [
+                    'Easily my favourite language',
+                    'Mostly, I\'ve used Kotlin for Android apps',
+                    'But as time went on, I started using it in other use cases as well, e.g. Spring Boot',
+                    'Going forward I definitely wanna improve my skills in that language!',
+                ],
             },
             {
                 title: 'Web development',
-                text:
-                    '- React/Typescript\n' +
-                    '- Javascript, HTML, CSS\n' +
-                    '- PHP\n' +
-                    'br\n',
+                text: [
+                    'React/Typescript',
+                    'JavaScript, HTML, CSS',
+                    'PHP',
+                ],
             },
             {
                 title: 'Other languages',
-                text:
-                    '- C# along Unity, for... well, Unity Games\n' +
-                    '- C++ for an Arduino/M5Stack project\n' +
-                    'br\n',
+                text: [
+                    'C# along Unity, for... well, Unity Games',
+                    'C++ for an Arduino/M5Stack project',
+                ],
             },
             {
                 title: 'Other technologies',
-                text:
-                    '> Git, GitHub, GitLab, BitBucket\n' +
-                    '> Jira, GitHub Projects\n' +
-                    '> IntelliJ, VSCode, Ubuntu\n' +
-                    '> GitHub Actions, GitLab CI\n' +
-                    '> Kubernetes (learning)\n' +
-                    'br\n',
+                text: [
+                    'Git, GitHub, GitLab, BitBucket',
+                    'Jira, GitHub Projects',
+                    'IntelliJ, VSCode, Ubuntu',
+                    'GitHub Actions, GitLab CI',
+                    'Kubernetes (learning)',
+                ],
             },
         ],
     },
